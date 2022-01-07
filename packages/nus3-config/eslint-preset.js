28e2cfd810b3b61ecdc4cac4b22b6e02546c06f1
nus3 <hadayota33@gmail.com>
2022-01-07 14:36:12 +0900

    :wrench: Add vscode setting

--- packages/nus3-config/eslint-preset.js
+++ packages/nus3-config/eslint-preset.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['next/core-web-vitals', 'prettier'],
   settings: {
     next: {
+      // REF: (nus3) https://nextjs.org/docs/basic-features/eslint#rootdir
       rootDir: ['apps/*/', 'packages/*/'],
     },
   },
