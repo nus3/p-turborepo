61a529b4b0955a16cecec4eb88e2f6e308e68317
nus3 <hadayota33@gmail.com>
2022-01-07 15:59:20 +0900

    :wrench: Add nus3-ui

---
+++ apps/nus3-a/next.config.js
@@ -0,0 +1,3 @@
+const withTM = require('next-transpile-modules')(['nus3-ui']);
+
+module.exports = withTM({});
--- apps/nus3-a/pages/index.tsx
+++ apps/nus3-a/pages/index.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next';
+import { Button } from 'nus3-ui';
 
 const IndexPage: NextPage = () => (
   <div>
     <h1>nus3-aっていうアプリケーションだよ</h1>
+    <Button />
   </div>
 );
 
