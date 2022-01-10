86166fb49630c9e875c49f170c5252ed1ab03cdb
nus3 <hadayota33@gmail.com>
2022-01-10 12:07:36 +0900

    :+1: Add nus3 button 2 in yarn

--- yarn/apps/nus3-a/pages/index.tsx
+++ yarn/apps/nus3-a/pages/index.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next';
 import { Button } from 'nus3-ui';
 import { Button as Nus3Button } from '@nus3/example-button';
+import { Button as Nus3Button2 } from '@nus3/example-button2';
 
 const IndexPage: NextPage = () => (
   <div>
     <h1>nus3-aっていうアプリケーションだよ</h1>
     <Button />
     <Nus3Button />
+    <Nus3Button2 />
   </div>
 );
 
---
+++ yarn/packages/nus3-button2/src/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <button>ワイはモノレポ内のnus3ボタンになるんや</button>;
+};
