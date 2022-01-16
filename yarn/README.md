ea7feb0734045e72f894b2d1320b00d09cfca0b1
nus3 <hadayota33@gmail.com>
2022-01-16 17:06:15 +0900

    :+1: Update in yarn

--- yarn/README.md
+++ yarn/README.md
@@ -3,5 +3,6 @@
 - apps: application
   - nus3-a: Next.js
 - packages: application で共通で使うもの
-  - nus3-config
-  - nus3-ui: React のコンポーネント置き場
+  - nus3-config: tsconfig + eslint の config
+  - nus3-ui: React のコンポーネント package(ビルドせずそのまま使う)
+  - nus3-button2: React + Vite でビルドするボタンコンポーネント package
--- yarn/packages/nus3-ui/Button.tsx
+++ yarn/packages/nus3-ui/Button.tsx
@@ -1,3 +1,14 @@
-export const Button = () => {
+import { Button as ExampleButton } from '@nus3/example-button2';
+import { VFC } from 'react';
+
+type ButtonProps = {
+  showExample?: boolean;
+};
+
+export const Button: VFC<ButtonProps> = ({ showExample }) => {
+  if (showExample) {
+    return <ExampleButton />;
+  }
+
   return <button>ボタンだよ</button>;
 };
